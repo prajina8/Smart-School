1a51673a0498e90d3d9f5062e3ce51d6a59ee85c
prajina-Adhikari <aprajina45@gmail.com>
2026-08-25 20:28:53 +0545

    changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import { SocketProvider } from "./context/SocketContext.jsx";
+import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <AuthProvider>
+        <SocketProvider>
+          <App />
+          <Toaster position="top-right" toastOptions={{ duration: 3500 }} />
+        </SocketProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
