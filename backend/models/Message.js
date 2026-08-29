54f3688148a6553a6ec5db57847d9b82a2b846ca
prajina-Adhikari <aprajina45@gmail.com>
2026-08-29 13:35:51 +0545

    new changes

---
+++ backend/models/Message.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    recipient: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    content: { type: String, required: true, trim: true },
+    read: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+messageSchema.index({ sender: 1, recipient: 1, createdAt: 1 });
+
+export default mongoose.model("Message", messageSchema);
